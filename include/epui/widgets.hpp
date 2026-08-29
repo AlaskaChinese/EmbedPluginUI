dee8a279e14246095ff98aa1a05ad928ee0b886e
Alaska Azusagawa <60684422+AlaskaChinese@users.noreply.github.com>
2026-08-29 11:45:44 +0800

    feat: add EmbedPluginUI widget API

---
+++ include/epui/widgets.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "openoledui/widgets.hpp"
+namespace epui {
+using openoledui::draw_header;
+using openoledui::draw_metric;
+using openoledui::draw_card;
+using openoledui::draw_wifi_icon;
+using openoledui::draw_thermometer;
+using openoledui::draw_spinner;
+}
